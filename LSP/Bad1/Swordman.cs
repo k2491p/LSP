df2bc3d3092ae47246c3887c16525b2e2fddfa95
k2491p <fukuda.dev10@gmail.com>
2022-03-10 22:25:00 +0900

    add Bad1 Example

---
+++ LSP/Bad1/Swordman.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LSP.Bad1
+{
+    public class Swordman
+    {
+        protected int _attack;
+        public Swordman(int attack)
+        {
+            _attack = attack;
+        }
+
+        public int AttackBySword => _attack;
+    }
+}
---
+++ LSP/Bad1/SwordmanMinarai.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LSP.Bad1
+{
+    public class SwordmanMinarai : Swordman
+    {
+        public SwordmanMinarai(int attack) : base(attack)
+        {
+        }
+
+        public int AttackBySword => (int)(_attack * 0.5);
+
+        public int AttackByWoodClub => _attack;
+    }
+}
